66ec06888117c7ef26d9b7a40e1bc9e037aafd65
Rengoan <91058472+Rengoan@users.noreply.github.com>
2023-06-02 15:55:51 +0200

    Update HotelNotFoundException.java

--- 11_Reserva/src/main/java/com/curso/exception/HotelNotFoundException.java
+++ 11_Reserva/src/main/java/com/curso/exception/HotelNotFoundException.java
@@ -2,6 +2,11 @@ package com.curso.exception;
 
 public class HotelNotFoundException extends RuntimeException{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1064484408553437919L;
+
 	public HotelNotFoundException(String message) {
 		
 		super(message);
