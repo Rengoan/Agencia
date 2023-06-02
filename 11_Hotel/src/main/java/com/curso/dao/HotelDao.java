d09043d2578740624d0d0fdb2569dafc59388c9e
Rengoan <91058472+Rengoan@users.noreply.github.com>
2023-06-02 11:14:24 +0200

    javadoc

--- 11_Hotel/src/main/java/com/curso/dao/HotelDao.java
+++ 11_Hotel/src/main/java/com/curso/dao/HotelDao.java
@@ -11,7 +11,11 @@ import org.springframework.data.jpa.repository.Query;
 import com.curso.model.Hotel;
 
 public interface HotelDao extends JpaRepository<Hotel, Integer> {
-
+/**
+ * Metodo personalizado para buscar un hotel por nombre
+ * @param nombre parametro para buscar por nombre
+ * @return
+ */
 	@Query("SELECT c FROM Hotel c WHERE c.nombre = :nombre")
 	Hotel findByName(String nombre);
 
